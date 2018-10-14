e6f57dc892cc926006526aa20aee20e58688ac45
Florian Rusch <git@florianrusch.de>
2018-10-14 22:15:12 +0200

    3: Cleanup run.py

--- server/run.py
+++ server/run.py
@@ -20,11 +20,8 @@ app.config['UPLOAD_FOLDER'] = './uploads'
 # Example
 # TODO remove this
 @app.route('/api/hello', methods=['POST'])
-def hello():
+def post_hello():
     return hello_world(prepare_request(app, request))
-
-# Example
-# TODO remove this
 
 
 @app.route('/api/twitter', methods=['POST'])
@@ -42,18 +39,11 @@ def get_instagram_data():
                                 input_data['firstname'], input_data['lastname'], input_data['email'])
     return jsonify(output_data)
 
+
 # For show uploaded pictures
-
-
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-
-# For show webpage
-@app.route('/')
-def index():
-    return send_from_directory('static', 'index.html')
 
 
 # Initiate flask
